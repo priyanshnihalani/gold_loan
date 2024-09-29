eccf3e81d162886296303dc3ea10d8e0ebd8ff99
priyanshnihalani <priyansh.nihalani@gmail.com>
2024-09-29 20:57:45 +0530

    Gold_Loan_Last_Step

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Use environment variables for Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAR2mw6erM0zEJp6xEGRn6Q_KrTriEYix4",
-  authDomain: "goldloan-dbe36.firebaseapp.com",
-  projectId: "goldloan-dbe36",
-  storageBucket: "goldloan-dbe36.appspot.com",
-  messagingSenderId: "242296268770",
-  appId: "1:242296268770:web:49b0a56cc9697bdf6a7bbb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getDatabase(app)
-export const firestoredb = getFirestore()
-export const provider = new GoogleAuthProvider()
+
+// Export Firebase services for use in other files
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const firestoredb = getFirestore(app);
+export const provider = new GoogleAuthProvider();
